ae654df800cb6fd80746b74bbb4b97893df894c4
Jason Cole <jaymcole2k@gmail.com>
2018-07-13 10:55:34 -0400

    Initial commit

---
+++ app/src/main/java/jasoncole/solitaire/MainActivity.kt
@@ -0,0 +1,12 @@
+package jasoncole.solitaire
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
